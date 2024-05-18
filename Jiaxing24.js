0d780d7f145234e60d98d93ff6e92aefb6f08fb6
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2024-05-18 00:27:04 -0400

    Update Jiaxing24.js

--- Jiaxing24.js
+++ Jiaxing24.js
@@ -51,6 +51,17 @@ turtlePond.bindPopup(`
     <img src="https://JohnasonY.github.io/GEG212Jiaxing/images/turtlePond.jpg" style="width: 100px; height: auto;">
 `).openPopup();
 
+// Add a marker to the map for New York City
+var MetropolitanMuseum = L.marker([40.779502442913945, -73.9632226876956]).addTo(map);
+
+// Add a popup to the marker
+MetropolitanMuseum.bindPopup(`
+    <b>The Metropolitan Museum of Art</b><br>Go visit! Free for NYS resident!<br>
+    <img src="https://JohnasonY.github.io/GEG212Jiaxing/images/MetropolitanMuseum.jpg" style="width: 100px; height: auto;">
+`).openPopup();
+
+
+
 
 
 // Load the GeoJSON line file
